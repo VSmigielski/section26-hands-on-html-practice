351f06bf83a53255096ec2df5f8bd1bda396c4f4
Veronica <veronicasmigielski@gmail.com>
2021-06-29 09:26:59 +0200

    Finished section26

---
+++ index.js
@@ -0,0 +1,25 @@
+function showPageSection(id) {
+    var listOfIds = ["home", "about-us", "menu", "contact"];
+    for (i=0; i < listOfIds.length; i++) {
+        if (listOfIds[i] == id) {
+            document.getElementById(id).style.visibility = "visible";
+        } else {
+            document.getElementById(listOfIds[i]).style.visibility = "hidden";
+        }
+    }
+}
+
+function itemOnClick(idOfItem, idOfAmount) {
+    var item = document.getElementById(idOfItem).innerHTML;
+    var amount = document.getElementById(idOfAmount).innerHTML;
+    var alertMessage = "Item: " + item + " Pizza" + "\n" + "Price: " + amount;
+    alert(alertMessage); 
+}
+
+function displayAlert() {
+    var emailAddress = document.getElementById("email").value;
+    var subjectLine = document.getElementById("subject").value;
+    var messageBody = document.getElementById("body").value;
+    var alertMessageBody = "Email: " + emailAddress + "\n" + "Subject: " + subjectLine + "\n" + "Body: " + messageBody;
+    alert(alertMessageBody);
+}
